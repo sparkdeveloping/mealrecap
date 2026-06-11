83fb4f9a5f1d2ac471f9af42cdd04dd54d4eec50
Denzel Nyatsanza <sbj.developing@gmail.com>
2026-06-11 03:09:40 -0500

    Initial Commit

---
+++ mealrecap/mealrecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealrecapApp.swift
+//  mealrecap
+//
+//  Created by Denzel Nyatsanza on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct mealrecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
